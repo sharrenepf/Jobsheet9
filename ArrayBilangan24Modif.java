2867593d66a064d882372b4bdf69db9f389f2396
Sharren <sharrenepf@gmail.com>
2024-10-31 13:31:45 +0700

    modif 1

---
+++ ArrayBilangan24Modif.java
@@ -0,0 +1,12 @@
+public class ArrayBilangan24Modif {
+    
+    public static void main(String[] args) {
+
+        int [] bil = {5, 23, -7, 17};
+
+        for (int i = 0; i < 4; i++) {
+            System.out.println(bil[i]);
+        }
+
+    }
+}
